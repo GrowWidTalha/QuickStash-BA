d0d88fc9e7baec02bf61cb7274093e51f3de30ac
Talha Ali <growwithtalha2@gmail.com>
2025-07-15 18:55:47 +0500

    fix: Swapped supabase instance with admin service key

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -18,7 +18,7 @@ export default database;
 
 export const supabase = createClient(
   process.env.SUPABASE_URL || '',
-  process.env.SUPABASE_ANON_KEY || ''
+  process.env.SUPABASE_SERVICE_KEY  || ''
 );
 
 export const config = {
